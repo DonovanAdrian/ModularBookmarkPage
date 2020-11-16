58b50aff154c714654ec4418e91f044f635ef930
DonovanAdrian <websurfer21020@gmail.com>
2020-11-15 22:46:47 -0700

    Updated main File
    Updated main JS file with navigation function.

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+function navigation(num) {
+    switch(num){
+        case 1:
+            window.location.href = "index.html";
+            break;
+        case 2:
+            window.location.href = "checklist.html";
+            break;
+        case 3:
+            window.location.href = "bookmark.html";
+            break;
+        case 4:
+            window.location.href = "DBIntegration.html";
+            break;
+        default:
+            break;
+    }
+}
